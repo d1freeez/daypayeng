b1c969fc5a0842a8f1a37d4985c460cec860167f
d1freeez <zhetpisbaevdidar@gmail.com>
2023-04-15 17:53:39 +0600

    eng

--- app/Http/Controllers/Pages/Auth/LoginController.php
+++ app/Http/Controllers/Pages/Auth/LoginController.php
@@ -13,7 +13,7 @@ class LoginController extends Controller
     public function index(): View
     {
         return view('pages.auth.login', [
-            'title' => 'Войти в аккаунт'
+            'title' => 'Sign in'
         ]);
     }
 
@@ -23,20 +23,20 @@ class LoginController extends Controller
             ->where('email', $request->get('email'))
             ->first();
         if (!$user) {
-            toastr('Введенный email не найден!', 'error');
+            toastr('Account email is not registered', 'error');
             return back();
         }
 
         if (!$user->getAttribute('is_active')) {
             toastr(
-                'Ваш аккаунт не активирован. Извиняемся за неудобства!',
+                'Your account has deactivated, sorry',
                 'error'
             );
             return back();
         }
 
         if (!auth()->attempt($request->only('email', 'password'))) {
-            toastr('Не правильный email/пароль', 'error');
+            toastr('Incorrect email/pass', 'error');
             return back();
         }
 
